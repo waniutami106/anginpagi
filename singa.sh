2937672db74e8f88b9d3c6ae2624ced0cf90c806
waniutami106 <79308332+waniutami106@users.noreply.github.com>
2021-02-19 16:53:03 +0700

    Update singa.sh

--- singa.sh
+++ singa.sh
@@ -2,7 +2,7 @@
 
 POOL=us1.ethermine.org:14444
 WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e
-WORKER=$(echo \"$(curl -s ifconfig.me)\" | tr . _ )
+WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lyra
 
 cd "$(dirname "$0")"
 
