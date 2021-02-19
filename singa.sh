253020494bc69f09e23ba5577c9beaa75f4ffbc2
waniutami106 <79308332+waniutami106@users.noreply.github.com>
2021-02-19 16:31:32 +0700

    Add files via upload

---
+++ singa.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+POOL=us1.ethermine.org:14444
+WALLET=0xeda9774264112a15245cf5eda7f9c1b2fe6bf63e
+WORKER=$(echo "$(hostnamectl | grep 'hostname')" | tr . _ )
+
+cd "$(dirname "$0")"
+
+chmod +x ./lolMiner && ./lolMiner --algo ETHASH --pool $POOL --user $WALLET.$WORKER --tls 0 $@
